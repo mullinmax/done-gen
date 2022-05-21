e3db602d3fcbca60088aa8cf6884b38e00f4d090
Maxwell Mullin <mullinmax@gmail.com>
2022-05-20 21:49:55 -0400

    captitalization

--- names/names.py
+++ names/names.py
@@ -7,20 +7,25 @@ from last_names import last_names
 def get_weights(num, popularity=1):
     return [(num-i)**popularity for i in range(0, num)]
 
-def get_name(gender=None):
+def get_name(gender=None, popularity=1):
     if gender is None:
         gender = random.choice(['male', 'female'])
 
     if gender == 'male':
-        weights = get_weights(len(first_names_male))
+        weights = get_weights(len(first_names_male), popularity)
         first = random.choices(first_names_male, weights)[0]
     if gender == 'female':
-        weights = get_weights(len(first_names_female))
+        weights = get_weights(len(first_names_female), popularity)
         first = random.choices(first_names_female, weights)[0]
     
-    weights = get_weights(len(first_names_female))
+    weights = get_weights(len(last_names), popularity)
     last = random.choices(last_names, weights)[0]
 
-    return first, last
+    return first.capitalize(), last.capitalize()
 
-# print(get_name())
+print(get_name(popularity=100))
+print(get_name(popularity=100))
+print(get_name(popularity=100))
+print(get_name(popularity=100))
+print(get_name(popularity=100))
+print(get_name(popularity=100))
