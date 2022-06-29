5d2409b9f05fbc2800e496a8a795cd7a78e27b6c
Maxwell Mullin <mullinmax@gmail.com>
2022-06-28 22:26:42 -0400

    basic working app

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #!/home/maxwell/miniconda3/envs/done-gen/bin/python
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
-from names import names
 app = Flask(__name__)
+
+@app.route('/')
+def home_route():
+    return render_template('index.html')
+
+from lists.names import names
 @app.route('/full-name')
-def index():
+def names_rounte():
     popularity = int(request.args.get('popularity', '1'))
     gender = request.args.get('gender', None)
     return jsonify(names.get_name(gender=gender, popularity=popularity))
--- setup_01.sh
+++ setup_01.sh
@@ -5,8 +5,7 @@ curl https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh 
 chmod +x miniconda_installer.sh
 ./miniconda_installer.sh -u
 rm miniconda_installer
+
 echo 'restart your shell then run setup_02.sh'
-# echo 'setting up conda enviroment'
-# conda create env -n done-gen python=3
-# source activate done-gen
-# conda install -r requirements.txt
+
+
