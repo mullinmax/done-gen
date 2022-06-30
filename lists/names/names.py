192548dc9493bf36e07562a39f0eeb439cffe8f6
Maxwell Mullin <mullinmax@gmail.com>
2022-06-29 23:09:05 -0400

    basic form started

--- lists/names/names.py
+++ lists/names/names.py
@@ -4,28 +4,21 @@ from .first_names_male import first_names_male
 from .first_names_female import first_names_female
 from .last_names import last_names
 
-def get_weights(num, popularity:int=1):
-    return [(num-i)**popularity for i in range(0, num)]
+def get_weights(num, diveristy:int=1):
+    return [(num-i)**(1/diveristy) for i in range(0, num)]
 
-def get_name(gender=None, popularity=1):
+def get_name(gender=None, diveristy=1):
     if gender is None:
         gender = random.choice(['male', 'female'])
 
     if gender == 'male':
-        weights = get_weights(len(first_names_male), popularity)
+        weights = get_weights(len(first_names_male), diveristy)
         first = random.choices(first_names_male, weights)[0]
     if gender == 'female':
-        weights = get_weights(len(first_names_female), popularity)
+        weights = get_weights(len(first_names_female), diveristy)
         first = random.choices(first_names_female, weights)[0]
     
-    weights = get_weights(len(last_names), popularity)
+    weights = get_weights(len(last_names), diveristy)
     last = random.choices(last_names, weights)[0]
 
     return first.capitalize(), last.capitalize()
-
-# print(get_name(popularity=100))
-# print(get_name(popularity=100))
-# print(get_name(popularity=100))
-# print(get_name(popularity=100))
-# print(get_name(popularity=100))
-# print(get_name(popularity=100))
--- main.py
+++ main.py
@@ -11,9 +11,19 @@ def home_route():
 from lists.names import names
 @app.route('/full-name')
 def names_rounte():
-    popularity = int(request.args.get('popularity', '1'))
+    diveristy = int(request.args.get('diveristy', '1'))
     gender = request.args.get('gender', None)
-    return jsonify(names.get_name(gender=gender, popularity=popularity))
+    return jsonify(names.get_name(gender=gender, diveristy=diveristy))
+
+@app.route('/npc', methods=["POST", "GET"])
+def npc():
+    title ='npc'
+    diveristy = float(request.form.get('diversity', '1'))
+    gender = request.form.get('gender', None)
+    print(diveristy, gender)
+    first, last = names.get_name(gender=gender, diveristy=diveristy)
+    return render_template('npc.html', first_name = first, last_name = last)
+
 
 if __name__ == "__main__":
     from waitress import serve
